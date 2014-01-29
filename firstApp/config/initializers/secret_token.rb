21e43a8f38d77f7a196a0cf25ead1456b0f5003e
unknown <C:\Users\tomaszewskim\AppData\Roaming\The Bat!>
2014-01-29 12:03:36 +0100

    Initializacja repozytorium

---
+++ firstApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'bf2b42543a06ad9b5975cfe58e254d82543ad25f35ac19a3c7428c38ab28420f7477c681973799197fddb234e3e15be3646b060775f948ac5ef7ad3456ff3911'
